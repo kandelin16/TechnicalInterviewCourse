3e3df291ab4db4c1d9d3946641c6fc95af6abfce
kandelin16 <78173482+kandelin16@users.noreply.github.com>
2022-01-12 13:30:18 -0700

    array hw

--- Class_04_Array_Problems_Easy/Array_Problems_Easy_Homework/Problems/best_time_to_buy_stock_problem.py
+++ Class_04_Array_Problems_Easy/Array_Problems_Easy_Homework/Problems/best_time_to_buy_stock_problem.py
@@ -5,3 +5,36 @@
 
 input_array = [7, 1, 5, 3, 6, 4]
 # Output = 5
+import math
+
+def TradeBot(array):
+    floatMin = [1000000000, 0]
+    floatMax = [0,0]
+    for index, price in enumerate(array):
+        if price < floatMin[0]:
+            floatMin = [price, index]
+        if price > floatMax[0] and index > floatMin[1]:
+            floatMax = [price, index]
+    return floatMax[0] - floatMin[0]
+
+def TradeBotOptimized(array):
+    floatMin = [math.inf, 0]
+    floatMax = [0,0]
+
+    if len(array) == 0:
+        return 0
+
+    for index, price in enumerate(array):
+        if price < floatMin[0]:
+            floatMin = [price, index]
+        if price > floatMax[0] and index > floatMin[1]:
+            floatMax = [price, index]
+    return floatMax[0] - floatMin[0]
+
+print(TradeBot(input_array))
+
+print(TradeBot([10,0,25,4,5]))
+
+print(TradeBotOptimized(input_array))
+
+print(TradeBotOptimized([10,0,25,4,5]))
--- Class_04_Array_Problems_Easy/Array_Problems_Easy_Homework/Problems/contains_duplicates_problem.py
+++ Class_04_Array_Problems_Easy/Array_Problems_Easy_Homework/Problems/contains_duplicates_problem.py
@@ -4,3 +4,27 @@
 
 input_array = [1, 2, 3, 3, 4]
 # Output = True
+
+def duplicates(array):
+    for i in range(0, len(array)):
+        for j in range(i+1, len(array)):
+            if array[i] == array[j]:
+                return True
+    
+    return False
+
+def duplicatesOptimal(array):
+    runningDict = {}
+    for int in array:
+        if int in runningDict:
+            return True
+        else:
+            runningDict[int] = 1
+    return False
+        
+
+print(duplicates(input_array))
+print(duplicates([1,2,3]))
+
+print(duplicatesOptimal(input_array))
+print(duplicatesOptimal([1,2,3]))
