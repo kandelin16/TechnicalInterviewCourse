6bba12c741e6e68a76d533079d09ce8a6a50cfea
kandelin16 <78173482+kandelin16@users.noreply.github.com>
2022-01-26 20:26:06 -0700

    more in class problems

--- Class_08_Dictionary_Problems_Medium/practice.py
+++ Class_08_Dictionary_Problems_Medium/practice.py
@@ -11,7 +11,30 @@ def twoSum(arr, targ):
         if ((targ-element) in dict):
             return dict[element], dict[targ-element]
 
-print(twoSum(arr, targ))
-print(twoSum(arr2, targ2))
+# print(twoSum(arr, targ))
+# print(twoSum(arr2, targ2))
 
 # {2:0, 7:1, 11:2, 15:3}
+
+sentence1 = "this apple is sweet"
+sentence2 = "this apple is sour"
+
+def uncommonWords(sen, sen2):
+    freq = {}
+    output = []
+    sen = sen.split(" ") + sen2.split(" ")
+    for element in sen: #9
+        if element in freq:
+            freq[element] +=1
+        else:
+            freq[element] = 1
+    for element in freq:
+        if freq[element] == 1:
+            output.append(element)
+
+    return(output)
+
+print(uncommonWords(sentence1, sentence2))
+
+# O(length of array 1 + length of array 2)
+# O(length of combined array)
